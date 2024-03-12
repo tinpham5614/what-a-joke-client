e712fb46903dd7275aeefa96b227a05fc3fa3d65
Tin Pham <nhattin55@gmail.com>
2024-03-11 23:22:05 -0700

    add temp logo

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,22 +1,21 @@
 "use client";
 
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import MenuIcon from '@mui/icons-material/Menu';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Link from 'next/link';
-import { Grid } from '@mui/material';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Link from "next/link";
+import { Grid } from "@mui/material";
+import logo from "../images/logo.jpg";
+import Image from "next/image";
 
 interface Props {
   /**
@@ -27,8 +26,8 @@ interface Props {
 }
 
 const drawerWidth = 240;
-const navItems = ['Home', 'About', 'Contact'];
-const linkItems = ['/', '/about', '/contact'];
+const navItems = ["Home", "About", "Contact"];
+const linkItems = ["/", "/about", "/contact"];
 
 export default function DrawerAppBar(props: Props) {
   const { window } = props;
@@ -39,16 +38,20 @@ export default function DrawerAppBar(props: Props) {
   };
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-      <Typography variant="h6" sx={{ my: 2 }}>
-        What A Joke
-      </Typography>
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Link href="/" passHref>
+        <Image src={logo} alt="What A Joke" />
+      </Link>
       <Divider />
       <List>
         {navItems.map((item, index) => (
           <Grid item key={item}>
             <Link href={linkItems[index]} passHref>
-              <Button key={item} sx={{ color: "#fff" }} onClick={handleDrawerToggle}>
+              <Button
+                key={item}
+                sx={{ color: "#fff" }}
+                onClick={handleDrawerToggle}
+              >
                 {item}
               </Button>
             </Link>
@@ -58,10 +61,11 @@ export default function DrawerAppBar(props: Props) {
     </Box>
   );
 
-  const container = window !== undefined ? () => window().document.body : undefined;
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
       <AppBar component="nav">
         <Toolbar>
@@ -70,19 +74,17 @@ export default function DrawerAppBar(props: Props) {
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: 'none' } }}
+            sx={{ mr: 2, display: { sm: "none" } }}
           >
             <MenuIcon />
           </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            What A Joke
-          </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
-          <Grid container spacing={2}>
+          <Box sx={{ flexGrow: 1 }}>
+          <Link href="/" passHref>
+            <Image src={logo} alt="What A Joke" width={100} height={50} />
+          </Link>
+          </Box>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            <Grid container spacing={2} justifyContent="flex-end">
               {navItems.map((item, index) => (
                 <Grid item key={item} sx={{ display: "flex" }}>
                   <Link href={linkItems[index]} passHref>
@@ -106,8 +108,11 @@ export default function DrawerAppBar(props: Props) {
             keepMounted: true, // Better open performance on mobile.
           }}
           sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
           }}
         >
           {drawer}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,35 @@
 
 import GetJoke from "./components/GetRandomJoke";
 import { Container, Divider, Grid } from "@mui/material";
-import CreateNewJoke from "./components/CreateNewJoke"; 
+import CreateNewJoke from "./components/CreateNewJoke";
 import Login from "./components/Login";
 import JokeCard from "./components/JokeCard";
 
 export default function Home() {
   return (
-    <Container fixed maxWidth="xl" sx={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
-    <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-      <Grid item xs={3}>
-        <Login />
+    <Container
+      fixed
+      maxWidth="xl"
+      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+    >
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        <Grid item xs={3}>
+          <Login />
+        </Grid>
+        <Grid item xs={6}>
+          <CreateNewJoke />
+          <Divider sx={{ marginY: 2 }} />
+          {/* Replace with GetJoke from backend */}
+          <JokeCard />
+        </Grid>
+        <Grid item xs={3}>
+          <GetJoke />
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <CreateNewJoke />
-        <Divider sx={{ marginY: 2}} />
-        {/* Replace with GetJoke from backend */}
-        <JokeCard />
-      </Grid>
-      <Grid item xs={3}>
-        <GetJoke />
-      </Grid>
-    </Grid>
-  </Container>
+    </Container>
   );
 }
