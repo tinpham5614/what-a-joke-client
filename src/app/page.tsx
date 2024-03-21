dc61827d9de83ceb64300d8dc33eefe70fd821d2
Tin Pham <nhattin55@gmail.com>
2024-03-20 23:30:20 -0700

    add floating button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { Container, Divider, Grid } from "@mui/material";
 import CreateNewJoke from "./dashboard/CreateNewJoke";
 import Login from "./dashboard/Login";
 import GetJokes from "./dashboard/GetJokes";
+import FloatingActionButtons from "./components/FloatingButton";
 
 export default function Home() {
   return (
@@ -29,6 +30,20 @@ export default function Home() {
         </Grid>
         <Grid item xs={3}>
           <GetRandomJoke />
+          <Divider sx={{ marginY: 2 }} />
+          <Grid
+            item
+            xs={12}
+            // set the button to the bottom right
+            sx={{
+              position: "fixed",
+              bottom: 0,
+              right: 0,
+              margin: 2,
+            }}
+          >
+            <FloatingActionButtons />
+          </Grid>
         </Grid>
       </Grid>
     </Container>
