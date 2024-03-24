93de55f363dbc576b5112dd2a3e41299695c9d74
Tin Pham <nhattin55@gmail.com>
2024-03-23 22:38:59 -0700

    update

--- tests/unit/math.test.ts
+++ tests/unit/math.test.ts
@@ -1,5 +1,5 @@
-import {add} from "../mocks/math";
+import { add } from "../mocks/math";
 
-test('add function adds two numbers correctly', () => {
-    expect(add(2, 3)).toBe(5);
+test("add function adds two numbers correctly", () => {
+  expect(add(2, 3)).toBe(5);
 });
