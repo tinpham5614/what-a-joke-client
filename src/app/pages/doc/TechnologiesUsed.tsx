5ea2a5849024fbbd9e2cc8f02ca3f416bedeca69
Tin Pham <nhattin55@gmail.com>
2024-03-28 18:53:29 -0700

    update technologies

--- src/app/pages/doc/TechnologiesUsed.tsx
+++ src/app/pages/doc/TechnologiesUsed.tsx
@@ -1,5 +1,37 @@
 import { Container, Typography, Paper } from "@mui/material";
+import Link from "next/link";
 import React from "react";
+
+const technologies = [
+  {
+    name: "Joke API",
+    url: "https://jokeapi.dev/",
+  },
+  {
+    name: "Next.js (Frontend)",
+    url: "https://nextjs.org/",
+  },
+  {
+    name: "Material-UI",
+    url: "https://mui.com/",
+  },
+  {
+    name: "NestJS (Backend)",
+    url: "https://nestjs.com/",
+  },
+  {
+    name: "MongoDB (Database)",
+    url: "https://www.mongodb.com/",
+  },
+  {
+    name: "Docker (Containerization)",
+    url: "https://www.docker.com/",
+  },
+  {
+    name: "Postman (API Testing)",
+    url: "https://www.postman.com/",
+  },
+];
 
 export default function TechnologiesUsed() {
   return (
@@ -9,13 +41,15 @@ export default function TechnologiesUsed() {
         This project uses the following technologies:
       </Typography>
       <Paper sx={{ p: 2, mt: 2 }}>
-        <Typography variant="body1">
-          - NextJS (Frontend)
-          <br />
-          - NestJS (Backend)
-          <br />
-          - MongoDB (Database)
-          <br />- Docker (Containerization)
+        <Typography variant="body1" component="div">
+          {technologies.map((tech, index) => (
+            <Container key={index} sx={{ mt: 1 }}>
+              {index + 1}.{" "}
+              <Link href={tech.url} target="_blank" rel="noopener noreferrer">
+                {tech.name}
+              </Link>
+            </Container>
+          ))}
         </Typography>
       </Paper>
     </Container>
