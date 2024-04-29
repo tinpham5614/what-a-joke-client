8abe950723fd3be1c2445f9ef023ab68e317991b
Tin Pham <nhattin55@gmail.com>
2024-04-29 16:32:58 -0700

    add tests

---
+++ __tests__/login.test.js
@@ -0,0 +1,16 @@
+import Login from "../src/app/auth/Login";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+describe("Login", () => {
+  it("renders a login form", () => {
+    render(<Login />);
+    const emailInput = screen.getByPlaceholderText("Email");
+    const passwordInput = screen.getByPlaceholderText("Password");
+    const submitButton = screen.getByRole("button", { name: "Login" });
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+});
