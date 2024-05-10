cd998ebfc905e3f14dd46edee63210a309c137a1
Tin Pham <nhattin55@gmail.com>
2024-05-09 23:52:46 -0700

    add additional API endpoints and mock user

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    env: {
+        NEXT_PUBLIC_API_URL:"http://localhost:3000/api",
+        NEXT_PUBLIC_AUTH_API_URL:"http://localhost:3000/api/auth",
+        NEXT_PUBLIC_JOKE_API_URL:"http://localhost:3000/api/jokes",
+        // vaiables for mock user
+        NEXT_PUBLIC_MOCK_USER_EMAIL:"admin@gmail.com",
+        NEXT_PUBLIC_MOCK_USER_PASSWORD:"qweqwe",
+    },
 };
 
 export default nextConfig;
