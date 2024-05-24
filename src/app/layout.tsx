e8014000a42f20e21ca5239841de718209944e37
Tin Pham <nhattin55@gmail.com>
2024-05-24 00:47:31 -0700

    modified themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      style={{ scrollBehavior: "smooth" }}
     >
       <body className={inter.className}>
         <ThemeContextProvider>
